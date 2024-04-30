5fccc023221382d12960b83813399f17f4d324a7
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-04-29 23:25:30 -0600

    controller basic empleado

---
+++ src/main/java/org/uv/DAPP02Pracica03/RepositoryEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/Repository.java to edit this template
+ */
+package org.uv.DAPP02Pracica03;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface RepositoryEmpleado extends JpaRepository<Empleado, Long> {
+    
+}
